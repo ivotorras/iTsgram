a8c0665885653dcbe04edf742c698b1297a16fcb
ivotorras <ivotorras1@gmail.com>
2016-12-01 13:39:15 -0300

    .

--- Gram/project/blog/urls.py
+++ Gram/project/blog/urls.py
@@ -8,15 +8,10 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
     url(r'^home/$', home, name='home'),
     url(r'^comentar/(\d+)$', comentar, name='comentar'),
-=======
-    url(r'^home/$', subir , name="subir" ),
-    url(r'^home/$', home),
     url(r'^home/(?P<username>[-\w]+)$', perfilajeno),
     url(r'^home/(?P<username>[-\w]+)/fav$', favs),
->>>>>>> 937d2bf0a42ad63bc4d5e936e42dc338f6a30697
     url(r'^register/$', register , name="register"),
     url(r'^register/success/$', register_success),
     url(r'^login/$', 'django.contrib.auth.views.login'),
